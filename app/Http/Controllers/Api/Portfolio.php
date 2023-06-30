e1693c70751d9c81a93a53234e8d0c1b0d6ca51a
Wesley Viana Martins <wesley.vmartins.js@gmail.com>
2023-06-30 05:49:25 -0300

    get repos on the github

--- app/Http/Controllers/Api/Portfolio.php
+++ app/Http/Controllers/Api/Portfolio.php
@@ -3,11 +3,21 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use GuzzleHttp\Client;
 
 class Portfolio extends Controller
 {
     public function index()
     {
-        return json_encode(['message' => 'Hello World']);
+        $client = new Client();
+        $token = env('GITHUB_TOKEN');
+        $response = $client->request('GET', 'https://api.github.com/users/wesleyvianam/repos', [
+            'headers' => [
+                'Authorization' => 'Bearer ' . $token,
+                'Accept' => 'application/vnd.github.v3+json',
+            ],
+        ]);
+
+        return $response->getBody();
     }
 }
