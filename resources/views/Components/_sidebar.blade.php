c45619923faf8ec324e56de050f47f955b028434
Wesley Viana Martins <wesley.vmartins.js@gmail.com>
2023-07-04 05:06:55 -0300

    Sidebar

--- resources/views/Components/_sidebar.blade.php
+++ resources/views/Components/_sidebar.blade.php
@@ -1,1 +1,20 @@
-<?php
+<style>
+    .sidebar {
+        width: 300px;
+        height: 300px;
+    }
+</style>
+
+<aside class="sidebar border shadow-sm me-3 p-3">
+    <h1 class="text-center pb-2 h5">Todo Flash</h1>
+
+    <ul class="nav nav-pills flex-column mb-auto">
+        <li class="nav-item">
+            <a href="#" class="nav-link {{ $active == 'task' ? 'active' : 'link-dark'}}">Task</a>
+        </li>
+
+        <li class="nav-item">
+            <a href="#" class="nav-link {{ $active == 'category' ? 'active' : 'link-dark'}}">Category</a>
+        </li>
+    </ul>
+</aside>
--- resources/views/Task/index.blade.php
+++ resources/views/Task/index.blade.php
@@ -1,15 +1,9 @@
 @extends('Components.app')
 
 @section('content')
-    <div style="max-width: 500px">
-        @if ($errors->any())
-            @foreach($errors->all() as $error)
-                <div class="alert alert-danger alert-dismissible fade show" role="alert">
-                    {{ $error }}
-                    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-                </div>
-            @endforeach
-        @endif
+
+    @include('Components._sidebar', ['active' => 'task'])
+    <div style="max-width: 600px">
 
         <form method="POST" action="{{ url('/task') }}">
             @csrf
@@ -23,28 +17,41 @@
                     <input class="form-control" type="text" name="category" id="category">
                     <label for="category">Category</label>
                 </div>
-                <button class="btn btn-primary">Salvar</button>
+                <button class="btn btn-primary px-3">Save</button>
             </div>
         </form>
 
         {{--INCOMPLETE--}}
         <ul class="list-group mb-3">
-            <li class="list-group-item d-flex justify-content-between">
-                <strong>Title</strong>
+            <li class="list-group-item d-flex justify-content-between align-items-center">
+                <strong style="width: 275px;">Title</strong>
                 <strong>Category</strong>
+                <strong>Action</strong>
             </li>
             @foreach($tasks as $task)
                 @if($task->status == "none")
-                    <li class="list-group-item d-flex justify-content-between px-3"
-                        style="text-decoration: {{ $task->status }}">
-                        <span>{{ $task->task }}</span>
+                    <li class="list-group-item d-flex justify-content-between align-items-center px-3">
+                        <span style="width: 300px">{{ $task->task }}</span>
                         <span>{{ $task->category }}</span>
                         <div>
-                            <a href="#" class="text-decoration-none btn btn-sm btn-white border" title="Complete">
-                                <i class="bi bi-check text-muted"></i>
-                            </a>
-                            <button class="btn btn-sm btn-white" title="Delete"><i
-                                    class="bi bi-trash-fill text-danger"></i></button>
+                            <form method="POST" action="{{ route('task.update', $task->id) }}" class="text-decoration-none btn btn-sm btn-white border" title="Complete">
+                                @csrf
+                                @Method('PUT')
+                                <input type="hidden" name="status" value="complete" />
+
+                                <button type="submit" class="btn btn-sm btn-white p-0" title="Complete">
+                                    <i class="bi bi-check text-muted"></i>
+                                </button>
+                            </form>
+                            <form method="POST" action="{{ route('task.destroy', $task->id) }}" class="text-decoration-none btn btn-sm btn-white border" title="Delete">
+                                @csrf
+                                @Method('DELETE')
+                                <input type="hidden" name="status" value="incomplete" />
+
+                                <button type="submit" class="btn btn-sm btn-white p-0" title="Delete">
+                                    <i class="bi bi-trash-fill text-danger"></i>
+                                </button>
+                            </form>
                         </div>
                     </li>
                 @endif
@@ -55,15 +62,29 @@
         <ul class="list-group">
             @foreach($tasks as $task)
                 @if($task->status != "none")
-                    <li class="list-group-item d-flex justify-content-between px-3 text-black-50">
-                        <span style="text-decoration: {{ $task->status }}">{{ $task->task }}</span>
+                    <li class="list-group-item d-flex justify-content-between align-items-center px-3 text-black-50">
+                        <span style="width: 300px; text-decoration: {{ $task->status }}">{{ $task->task }}</span>
                         <span style="text-decoration: {{ $task->status }}">{{ $task->category }}</span>
                         <div>
-                            <a href="#" class="text-decoration-none btn btn-sm btn-white border" title="Rollback">
-                                <i class="bi bi-arrow-counterclockwise"></i>
-                            </a>
-                            <button class="btn btn-sm btn-white" title="Delete"><i
-                                    class="bi bi-trash-fill text-danger"></i></button>
+                            <form method="POST" action="{{ route('task.update', $task->id) }}" class="text-decoration-none btn btn-sm btn-white border" title="Rollback">
+                                @csrf
+                                @Method('PUT')
+                                <input type="hidden" name="status" value="incomplete" />
+
+                                <button type="submit" class="btn btn-sm btn-white p-0" title="Rollback">
+                                    <i class="bi bi-arrow-counterclockwise text-muted"></i>
+                                </button>
+                            </form>
+
+                            <form method="POST" action="{{ route('task.destroy', $task->id) }}" class="text-decoration-none btn btn-sm btn-white border" title="Delete">
+                                @csrf
+                                @Method('DELETE')
+                                <input type="hidden" name="status" value="incomplete" />
+
+                                <button type="submit" class="btn btn-sm btn-white p-0" title="Delete">
+                                    <i class="bi bi-trash-fill text-danger"></i>
+                                </button>
+                            </form>
                         </div>
                     </li>
                 @endif
