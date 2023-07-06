099f8f857b8f8fd61cb75d7c82e643c1ea055466
Wesley Viana Martins <wesley.vmartins.js@gmail.com>
2023-07-06 06:26:14 -0300

    update style

--- resources/views/Project/index.blade.php
+++ resources/views/Project/index.blade.php
@@ -6,8 +6,8 @@
 
     <div style="width: 600px">
         <div class="d-flex justify-content-between pb-3">
-            <input placeholder="Search" class="form-control" style="width: 500px">
-            <a href="{{ route('project.create') }}" class="btn btn-primary">New</a>
+            <input placeholder='Search' class="form-control" style="width: 515px">
+            <a href="{{ route('project.create') }}" class="btn btn-danger">New</a>
         </div>
 
         <ul class="list-group">
