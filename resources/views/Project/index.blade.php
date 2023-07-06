4fd34635cad6d2d07f1a8ea1c4821f9334f0acf4
wesley <wesley.vmartins.js@gmail.com>
2023-07-06 12:25:38 -0300

    created modal of insert new project in database

--- resources/views/Project/index.blade.php
+++ resources/views/Project/index.blade.php
@@ -7,7 +7,48 @@
     <div style="width: 600px">
         <div class="d-flex justify-content-between pb-3">
             <input placeholder='Search' class="form-control" style="width: 515px">
-            <a href="{{ route('project.create') }}" class="btn btn-danger">New</a>
+
+            <button type="button" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                New
+            </button>
+
+            <!-- Modal -->
+            <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div class="modal-dialog">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h1 class="modal-title fs-5" id="exampleModalLabel">Create a new Project</h1>
+                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div class="modal-body">
+                            <form method="POST" action="{{ route('project.store') }}">
+                                @csrf
+                                <div class="d-flex pb-3">
+                                    <div class="pe-3" style="width: 420px">
+                                        <input class="form-control" type="text" name="title" id="title" placeholder="Title">
+                                    </div>
+
+                                    <select style="width: 120px" class="form-select" name="type" id="type">
+                                        <option value="list">list</option>
+                                        <option value="kanban">kanban</option>
+                                    </select>
+                                </div>
+
+                                <div class="row">
+                                    <div class="col">
+                                        <textarea class="form-control" name="description" id="description"></textarea>
+                                    </div>
+                                </div>
+
+                                <div class="mt-3 text-end">
+                                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                    <button class="btn btn-primary px-3">Save</button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
 
         <ul class="list-group">
