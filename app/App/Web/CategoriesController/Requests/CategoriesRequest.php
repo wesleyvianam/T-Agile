2b7295f8e3e512664586eb2ac4c985bd22531f4a
Wesley Viana Martins <wesley.vmartins.js@gmail.com>
2023-07-07 06:00:08 -0300

    Created rules of request of category

--- app/App/Web/CategoriesController/Requests/CategoriesRequest.php
+++ app/App/Web/CategoriesController/Requests/CategoriesRequest.php
@@ -2,7 +2,14 @@
 
 namespace App\Web\CategoriesController\Requests;
 
-class CategoriesRequest
-{
+use Illuminate\Foundation\Http\FormRequest;
 
+class CategoriesRequest extends FormRequest
+{
+    public function rules(): array
+    {
+        return [
+            'title' => ['required', 'string'],
+        ];
+    }
 }
