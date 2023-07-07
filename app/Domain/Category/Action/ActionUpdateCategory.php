911c0612bdf2c065deab9a05be68af75ce794946
Wesley Viana Martins <wesley.vmartins.js@gmail.com>
2023-07-07 05:57:29 -0300

    Created action update of category

--- app/Domain/Category/Action/ActionUpdateCategory.php
+++ app/Domain/Category/Action/ActionUpdateCategory.php
@@ -2,7 +2,16 @@
 
 namespace Domain\Category\Action;
 
+use Domain\Category\DataTransferObjects\CategoryCreateData;
+use Domain\Category\Models\Category;
+
 class ActionUpdateCategory
 {
-
+    public function __invoke(Category $category, CategoryCreateData $data): bool
+    {
+        return $category->update([
+            'title' => $data->title,
+            'description' => $data->description
+        ]);
+    }
 }
