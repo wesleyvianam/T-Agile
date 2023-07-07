652d838bd46bb9c5210cf89e36d500e05bae24ce
Wesley Viana Martins <wesley.vmartins.js@gmail.com>
2023-07-07 05:57:16 -0300

    Created action create of category

--- app/Domain/Category/Action/ActionCreateCategory.php
+++ app/Domain/Category/Action/ActionCreateCategory.php
@@ -2,7 +2,19 @@
 
 namespace Domain\Category\Action;
 
+use Domain\Category\DataTransferObjects\CategoryCreateData;
+use Domain\Category\Models\Category;
+
 class ActionCreateCategory
 {
-
+    public function __invoke(CategoryCreateData $data): Category
+    {
+        return Category::create([
+            'title' => $data->title,
+            'description' => $data->description
+        ]);
+    }
 }
+
+
+
