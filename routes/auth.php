c975a9fc16086b06ae5c1b1fc7865541be619276
Wesley Viana Martins <wesley.vmartins.js@gmail.com>
2023-07-10 05:07:20 -0300

    init clean Architecture

--- routes/auth.php
+++ routes/auth.php
@@ -1,14 +1,14 @@
 <?php
 
-use App\Http\Controllers\Auth\AuthenticatedSessionController;
-use App\Http\Controllers\Auth\ConfirmablePasswordController;
-use App\Http\Controllers\Auth\EmailVerificationNotificationController;
-use App\Http\Controllers\Auth\EmailVerificationPromptController;
-use App\Http\Controllers\Auth\NewPasswordController;
-use App\Http\Controllers\Auth\PasswordController;
-use App\Http\Controllers\Auth\PasswordResetLinkController;
-use App\Http\Controllers\Auth\RegisteredUserController;
-use App\Http\Controllers\Auth\VerifyEmailController;
+use App\Core\Http\Controllers\Auth\AuthenticatedSessionController;
+use App\Core\Http\Controllers\Auth\ConfirmablePasswordController;
+use App\Core\Http\Controllers\Auth\EmailVerificationNotificationController;
+use App\Core\Http\Controllers\Auth\EmailVerificationPromptController;
+use App\Core\Http\Controllers\Auth\NewPasswordController;
+use App\Core\Http\Controllers\Auth\PasswordController;
+use App\Core\Http\Controllers\Auth\PasswordResetLinkController;
+use App\Core\Http\Controllers\Auth\RegisteredUserController;
+use App\Core\Http\Controllers\Auth\VerifyEmailController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('guest')->group(function () {
