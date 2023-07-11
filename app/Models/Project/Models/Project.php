b15c8372e1cc6327d4d23436669cbfb1d705590f
Wesley Viana Martins <wesley.vmartins.js@gmail.com>
2023-07-11 06:23:27 -0300

    created model Project

--- app/Models/Project/Models/Project.php
+++ app/Models/Project/Models/Project.php
@@ -2,7 +2,12 @@
 
 namespace App\Models\Project\Models;
 
-class Project
-{
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 
+class Project extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['title', 'user_id'];
 }
