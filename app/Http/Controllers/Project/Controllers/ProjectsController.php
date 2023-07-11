c09ec42fad55e6b39e78a6d582bbc5fe81cb8196
Wesley Viana Martins <wesley.vmartins.js@gmail.com>
2023-07-11 06:25:05 -0300

    created Project Controller

--- app/Http/Controllers/Project/Controllers/ProjectsController.php
+++ app/Http/Controllers/Project/Controllers/ProjectsController.php
@@ -1,19 +1,20 @@
 <?php
 
-namespace App\Http\Controllers\Project;
+namespace App\Http\Controllers\Project\Controllers;
 
 use App\Http\Controllers\Controller;
 use App\Http\Controllers\Project\Resquests\RequestProjects;
 use App\Models\Project\Actions\CreateProjectsAction;
 use App\Models\Project\DTO\ProjectCreateData;
 use App\Models\Project\Models\Project;
-use Illuminate\Support\Facades\Auth;
 
 class ProjectsController extends Controller
 {
     public function index()
     {
-        $projects = Project::all();
+        $userId = auth()->user()->getAuthIdentifier();
+
+        $projects = Project::where('user_id', $userId)->get();
 
         return view('project.index')->with('projects', $projects);
     }
