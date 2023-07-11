d20a289e9513d408b680c7af8f68b38a2cd466d6
Wesley Viana Martins <wesley.vmartins.js@gmail.com>
2023-07-11 06:22:22 -0300

    update migration of project para projects

--- database/migrations/2023_07_11_074209_create_projects.php
+++ database/migrations/2023_07_11_074209_create_projects.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('projects', function (Blueprint $table) {
+        Schema::create('project', function (Blueprint $table) {
             $table->id();
             $table->string('title');
 
@@ -27,6 +27,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('projects');
+        Schema::dropIfExists('project');
     }
 };
