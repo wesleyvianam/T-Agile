7bf46de845c662a3d9b9af647282b19e6ebf78c7
Wesley Viana Martins <wesley.vmartins.js@gmail.com>
2023-07-11 20:54:42 -0300

    update migrate task

--- database/migrations/2023_07_11_205853_create_tasks.php
+++ database/migrations/2023_07_11_205853_create_tasks.php
@@ -13,6 +13,18 @@ return new class extends Migration
     {
         Schema::create('tasks', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->string('status');
+
+            $table->unsignedBigInteger('project_id');
+            $table->foreign('project_id')->references('id')->on('projects');
+
+            $table->unsignedBigInteger('epic_id');
+            $table->foreign('epic_id')->references('id')->on('epics');
+
+            $table->unsignedBigInteger('category_id');
+            $table->foreign('category_id')->references('id')->on('category');
+
             $table->timestamps();
         });
     }
