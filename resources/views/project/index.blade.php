7c23559cc655eb32da2156d60800d385ed1e07b7
wesley <wesley.vmartins.js@gmail.com>
2023-07-11 12:39:36 -0300

    created options edit and destroy for project

--- resources/views/project/index.blade.php
+++ resources/views/project/index.blade.php
@@ -11,13 +11,25 @@
 
         <ul class="mt-3">
             @foreach($projects as $project)
-                <li class="border p-2 rounded">
-                    {{ $project->user_id }}
-                    {{ $project->title }}
+                <li class="border p-2 rounded flex justify-between">
+                    <div>
+                        <span>{{ $project->user_id }}</span>
+                        <span>{{ $project->title }}</span>
+                    </div>
+
+                    <div class="flex">
+                        <a href="{{ route('project.edit', $project->id) }}" class="pe-3">
+                            <i class="bi bi-pencil-square text-gray-500 hover:text-gray-800"></i>
+                        </a>
+                        <form action="{{ route('project.destroy', $project->id) }}" method="post">
+                            @csrf
+                            @method('Delete')
+
+                            <button><i class="text-red-500 hover:text-red-800 bi bi-trash-fill"></i></button>
+                        </form>
+                    </div>
                 </li>
             @endforeach
         </ul>
     </div>
-
-
 @endsection
