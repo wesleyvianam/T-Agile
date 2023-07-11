90d9bcca74a8acfcf8d9517882a4d1360191799c
Wesley Viana Martins <wesley.vmartins.js@gmail.com>
2023-07-11 06:17:33 -0300

    created new component default

--- resources/views/components/app.blade.php
+++ resources/views/components/app.blade.php
@@ -1,1 +1,32 @@
-<?php
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>@yield('title')</title>
+
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
+
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+<body class="flex justify-center" style="padding: 5rem;">
+    @if(session('success'))
+        <div>{{ session('success') }}</div>
+    @endif
+
+    @if($errors->any())
+        <ul>
+            @foreach($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    @endif
+
+    <div style="width: 900px">
+        @yield('content')
+    </div>
+</body>
+</html>
