ecc1f25d6d5cecc291e02797a44713689666118d
Wesley Viana Martins <wesley.vmartins.js@gmail.com>
2023-07-11 06:24:21 -0300

    Created DTO Project

--- app/Models/Project/DTO/ProjectCreateData.php
+++ app/Models/Project/DTO/ProjectCreateData.php
@@ -2,7 +2,13 @@
 
 namespace App\Models\Project\DTO;
 
-class ProjectCreateData
-{
+use Spatie\LaravelData\Data;
 
+class ProjectCreateData extends Data
+{
+    public function __construct(
+        public readonly string $title,
+        public readonly int $userId,
+    ) {
+    }
 }
