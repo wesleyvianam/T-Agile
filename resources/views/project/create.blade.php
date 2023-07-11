e2f639fcc67d03e2106dffbdb994541c22844818
Wesley Viana Martins <wesley.vmartins.js@gmail.com>
2023-07-11 06:18:49 -0300

    Created index project

--- resources/views/project/create.blade.php
+++ resources/views/project/create.blade.php
@@ -1,1 +1,25 @@
-<?php
+@extends('components.app')
+
+@section('title') Create Project @endsection
+
+@section('content')
+    <div>
+        <div class="flex justify-between">
+            <h1>New Project</h1>
+            <a href="{{ route('project.index') }}" class="border p-2">voltar</a>
+        </div>
+
+        <div>
+            <form action="{{ route('project.store') }}" method="post">
+                @csrf
+
+                <input name="userId" id="userId" type="hidden" value="{{ Auth::id() }}">
+
+                <input name="title" id="title" type="text" placeholder="Title">
+
+                <button class="border p-2">Salvar</button>
+            </form>
+        </div>
+    </div>
+
+@endsection
