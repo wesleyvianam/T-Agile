d7acda816ae8e0946d1d81fde5224913b0febd21
Wesley Viana Martins <wesley.vmartins.js@gmail.com>
2023-07-11 06:19:19 -0300

    Created pages project

--- resources/views/project/index.blade.php
+++ resources/views/project/index.blade.php
@@ -1,1 +1,23 @@
-<?php
+@extends('components.app')
+
+@section('title') Project @endsection
+
+@section('content')
+    <div class="">
+        <div class="flex justify-between">
+            <h1>Project</h1>
+            <a href="{{ route('project.create') }}" class="border p-2">New Project</a>
+        </div>
+
+        <ul class="mt-3">
+            @foreach($projects as $project)
+                <li class="border p-2 rounded">
+                    {{ $project->user_id }}
+                    {{ $project->title }}
+                </li>
+            @endforeach
+        </ul>
+    </div>
+
+
+@endsection
