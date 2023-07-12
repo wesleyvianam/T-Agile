683482e883ef97a3f0b15fdf79e14250e3ada788
Wesley Viana Martins <wesley.vmartins.js@gmail.com>
2023-07-11 21:15:17 -0300

    created Request ProfilesController.php

---
+++ app/Http/Controllers/Profile/Requests/RequestProfile.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers\Profile\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class RequestProfile extends FormRequest
+{
+    public function rules(): array
+    {
+        return [];
+    }
+}
