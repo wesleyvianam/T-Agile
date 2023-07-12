658f2323344219ec1b30e2d33c88780e552b71d5
Wesley Viana Martins <wesley.vmartins.js@gmail.com>
2023-07-12 06:12:31 -0300

    Created relations of Project

--- app/Models/Project/Models/Project.php
+++ app/Models/Project/Models/Project.php
@@ -2,6 +2,9 @@
 
 namespace App\Models\Project\Models;
 
+use App\Models\Category\Models\Category;
+use App\Models\Epic\Models\Epic;
+use App\Models\Task\Models\Task;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -10,4 +13,19 @@ class Project extends Model
     use HasFactory;
 
     protected $fillable = ['title', 'user_id'];
+
+    public function epics()
+    {
+        return $this->hasMany(Epic::class, 'project_id');
+    }
+
+    public function tasks()
+    {
+        return $this->hasMany(Task::class, 'project_id');
+    }
+
+    public function categories()
+    {
+        return $this->hasMany(Category::class, 'project_id');
+    }
 }
