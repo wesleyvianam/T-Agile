7d8408d3e55260a5c0faf9c86680b9f218c5da73
Wesley Viana Martins <wesley.vmartins.js@gmail.com>
2023-07-11 21:15:41 -0300

    created Request PositionsController.php

---
+++ app/Http/Controllers/Position/Requests/RequestPosition.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers\Position\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class RequestPosition extends FormRequest
+{
+    public function rules(): array
+    {
+        return [];
+    }
+}
