ff77569ac744fcb3f5fb51025c2689f8f97fbf59
Wesley Viana Martins <wesley.vmartins.js@gmail.com>
2023-07-12 05:45:53 -0300

    Created index and create Epic

--- app/Http/Controllers/Epic/Controllers/EpicsController.php
+++ app/Http/Controllers/Epic/Controllers/EpicsController.php
@@ -3,15 +3,26 @@
 namespace App\Http\Controllers\Epic\Controllers;
 
 use App\Http\Controllers\Controller;
+use App\Models\Epic\Models\Epic;
+use App\Models\Project\Models\Project;
+use Illuminate\Http\Request;
 
 class EpicsController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
+        $projectId = $request->query('id');
+
+        $epics = Epic::where('project_id', $projectId)->get();
+
+        return view('epic.index', compact('epics', 'projectId'));
     }
 
-    public function create()
+    public function create(Request $request)
     {
+        $projectId = $request->query('id');
+
+        return view('epic.create', compact('projectId'));
     }
 
     public function store()
