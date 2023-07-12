30d72c60c4f91e958bc3b5455e7f9092246eb854
Wesley Viana Martins <wesley.vmartins.js@gmail.com>
2023-07-12 06:05:18 -0300

    Update Action Create Epic fo save data

--- app/Models/Epic/Actions/CreateEpicAction.php
+++ app/Models/Epic/Actions/CreateEpicAction.php
@@ -2,10 +2,16 @@
 
 namespace App\Models\Epic\Actions;
 
+use App\Models\Epic\DTO\EpicCreateData;
+use App\Models\Epic\Models\Epic;
+
 final class CreateEpicAction
 {
-    public function __invoke()
+    public function __invoke(EpicCreateData $data)
     {
-        // TODO: Implement __invoke() method.
+        return Epic::create([
+            'title' => $data->title,
+            'project_id' => $data->projectId,
+        ]);
     }
 }
