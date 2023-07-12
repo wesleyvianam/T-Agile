8e992e5bfe7f26e3ba848d973008fe76991a4cf0
wesley <wesley.vmartins.js@gmail.com>
2023-07-12 10:34:30 -0300

    Update CategoryController

--- app/Http/Controllers/Category/Controllers/CategoriesController.php
+++ app/Http/Controllers/Category/Controllers/CategoriesController.php
@@ -2,24 +2,48 @@
 
 namespace App\Http\Controllers\Category\Controllers;
 
+use App\Http\Controllers\Category\Requests\RequestCategory;
 use App\Http\Controllers\Controller;
+use App\Models\Category\Actions\CreateCategoryAction;
+use App\Models\Category\DTO\CategoryCreateData;
+use App\Models\Category\Models\Category;
+use Illuminate\Http\Request;
 
 class CategoriesController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
+        $projectId = $request->query('id');
+
+        $categories = Category::where('project_id', $projectId)->get();
+
+        return view('category.index', compact('categories', 'projectId'));
     }
 
-    public function create()
+    public function create(Request $request)
     {
+        $projectId = $request->query('id');
+
+        return view('category.form', compact('projectId'));
     }
 
-    public function store()
+    public function store(RequestCategory $request, CreateCategoryAction $action)
     {
+        $title = $request->get('title');
+        $projectId = $request->get('projectId');
+
+        $data = new CategoryCreateData($title, $projectId);
+
+        $action($data);
+
+        return to_route('category.index')->with('id', $projectId);
     }
 
-    public function edit()
+    public function edit(Category $category, Request $request)
     {
+        $projectId = $request->query('id');
+
+        return view('category.form', compact('category','projectId'));
     }
 
     public function update()
