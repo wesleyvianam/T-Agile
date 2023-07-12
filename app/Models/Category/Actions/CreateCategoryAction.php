85b2c5d93345012867a48c790cc662873ea5cd1e
wesley <wesley.vmartins.js@gmail.com>
2023-07-12 10:34:58 -0300

    Update Action Create Category

--- app/Models/Category/Actions/CreateCategoryAction.php
+++ app/Models/Category/Actions/CreateCategoryAction.php
@@ -2,10 +2,16 @@
 
 namespace App\Models\Category\Actions;
 
+use App\Models\Category\DTO\CategoryCreateData;
+use App\Models\Category\Models\Category;
+
 final class CreateCategoryAction
 {
-    public function __invoke()
+    public function __invoke(CategoryCreateData $data): bool
     {
-        // TODO: Implement __invoke() method.
+        return Category::create([
+            'title' => $data->title,
+            'project_id' => $data->title
+        ]);
     }
 }
