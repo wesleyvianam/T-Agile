230f586e77e4301c9e53971dad061da5df8842fd
Wesley Viana Martins <wesley.vmartins.js@gmail.com>
2023-07-11 21:15:51 -0300

    created Request Mermbers

---
+++ app/Http/Controllers/Member/Requests/RequestMember.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers\Member\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class RequestMember extends FormRequest
+{
+    public function rules(): array
+    {
+        return [];
+    }
+}
