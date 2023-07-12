281062b89374026c064ba7a0166b447715562eff
wesley <wesley.vmartins.js@gmail.com>
2023-07-12 10:35:12 -0300

    Update DTO Category

--- app/Models/Category/DTO/CategoryCreateData.php
+++ app/Models/Category/DTO/CategoryCreateData.php
@@ -6,7 +6,9 @@ use Spatie\LaravelData\Data;
 
 class CategoryCreateData extends Data
 {
-    public function __construct()
-    {
+    public function __construct(
+        public readonly string $title,
+        public readonly int $projectId
+    ){
     }
 }
