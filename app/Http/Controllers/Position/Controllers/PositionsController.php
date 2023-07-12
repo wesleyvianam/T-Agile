0662cd1b7faac731be178c795718f06eeec58379
Wesley Viana Martins <wesley.vmartins.js@gmail.com>
2023-07-11 21:16:21 -0300

    created controller positions

---
+++ app/Http/Controllers/Position/Controllers/PositionsController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers\Position\Controllers;
+
+use App\Http\Controllers\Controller;
+
+class PositionsController extends Controller
+{
+    public function index()
+    {
+    }
+
+    public function create()
+    {
+    }
+
+    public function store()
+    {
+    }
+
+    public function edit()
+    {
+    }
+
+    public function update()
+    {
+    }
+
+    public function destroy()
+    {
+    }
+}
