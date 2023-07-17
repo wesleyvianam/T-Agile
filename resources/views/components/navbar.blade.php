dd23c35c6fc35c7f2148840b05e6000e3c7034fe
Wesley Viana Martins <wesley.vmartins.js@gmail.com>
2023-07-17 06:12:53 -0300

    improvements in navbar

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,9 +1,19 @@
-<nav class="w-full border-b mb-10 p-3 px-80">
-    <h1>
-        T-Agile
-    </h1>
+<nav class="w-full mb-10 p-3 px-80 border-b">
+    <div class="flex justify-between items-center">
+        <h1 class="text-2xl font-bold text-primary">
+            T-Agile
+        </h1>
 
-    <div>
+        <div>
+            @auth
+                <a href="{{ url('/dashboard') }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Dashboard</a>
+            @else
+                <a href="{{ route('login') }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Log in</a>
 
+                @if (Route::has('register'))
+                    <a href="{{ route('register') }}" class="ml-4 font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Register</a>
+                @endif
+            @endauth
+        </div>
     </div>
 </nav>
