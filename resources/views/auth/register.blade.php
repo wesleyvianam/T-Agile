6d6de318dd9fbaf930dbf8f5ff703eac2ab39c94
Wesley Viana Martins <wesley.vmartins.js@gmail.com>
2023-07-18 05:49:29 -0300

    Created register page

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,52 +1,51 @@
-<x-guest-layout>
-    <form method="POST" action="{{ route('register') }}">
-        @csrf
+@extends('components.app')
 
-        <!-- Name -->
-        <div>
-            <x-input-label for="name" :value="__('Name')" />
-            <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
-            <x-input-error :messages="$errors->get('name')" class="mt-2" />
+@section('title') Login | T-Agile @endsection
+
+@section('content')
+    <div class="w-full flex justify-between items-center">
+        <div class="shadow w-1/2 p-6 rounded-xl">
+            <div>
+                <h1 class="mb-3 text-4xl font-bold text-center">Register</h1>
+                <form>
+                    <div class="mb-3">
+                        <label for="website-admin" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Name</label>
+                        <input type="text" id="website-admin" class="rounded-lg bg-gray-50 text-gray-900 focus:border-none block flex-1 min-w-0 w-full text-sm p-2.5  dark:bg-gray-700" placeholder="Name">
+                    </div>
+
+                    <div class="mb-3">
+                        <label for="website-admin" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Username</label>
+                        <input type="text" id="website-admin" class="rounded-lg bg-gray-50 text-gray-900 focus:border-none block flex-1 min-w-0 w-full text-sm p-2.5  dark:bg-gray-700" placeholder="Email">
+                    </div>
+
+                    <div class="mb-3">
+                        <label for="website-admin" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Email</label>
+                        <input type="text" id="website-admin" class="rounded-lg bg-gray-50 text-gray-900 focus:border-none block flex-1 min-w-0 w-full text-sm p-2.5  dark:bg-gray-700" placeholder="Email">
+                    </div>
+
+                    <div class="mb-3">
+                        <label for="website-admin" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
+                        <input type="text" id="website-admin" class="rounded-lg bg-gray-50 text-gray-900 focus:border-none block flex-1 min-w-0 w-full text-sm p-2.5  dark:bg-gray-700" placeholder="Password">
+                    </div>
+
+                    <div class="mb-5">
+                        <label for="website-admin" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Confirm Password</label>
+                        <input type="text" id="website-admin" class="rounded-lg bg-gray-50 text-gray-900 focus:border-none block flex-1 min-w-0 w-full text-sm p-2.5  dark:bg-gray-700" placeholder="Confirm Password">
+                    </div>
+
+                    <div>
+                        <div class="flex justify-between items-center">
+                            <p class="">Já possui conta? <a href="{{ route('login') }}" class="text-primary">Entrar</a></p>
+
+                            <button class="mb-5 py-2 px-4 border rounded-md">Registrar</button>
+                        </div>
+                    </div>
+                </form>
+            </div>
         </div>
 
-        <!-- Email Address -->
-        <div class="mt-4">
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autocomplete="username" />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
+        <div class="w-1/2 relative left-20">
+            <img width="600" src="/image/scrum.jpg" alt="Banner de representação do framework ágil Scrum" />
         </div>
-
-        <!-- Password -->
-        <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
-
-            <x-text-input id="password" class="block mt-1 w-full"
-                            type="password"
-                            name="password"
-                            required autocomplete="new-password" />
-
-            <x-input-error :messages="$errors->get('password')" class="mt-2" />
-        </div>
-
-        <!-- Confirm Password -->
-        <div class="mt-4">
-            <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
-
-            <x-text-input id="password_confirmation" class="block mt-1 w-full"
-                            type="password"
-                            name="password_confirmation" required autocomplete="new-password" />
-
-            <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
-        </div>
-
-        <div class="flex items-center justify-end mt-4">
-            <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('login') }}">
-                {{ __('Already registered?') }}
-            </a>
-
-            <x-primary-button class="ml-4">
-                {{ __('Register') }}
-            </x-primary-button>
-        </div>
-    </form>
-</x-guest-layout>
+    </div>
+@endsection
