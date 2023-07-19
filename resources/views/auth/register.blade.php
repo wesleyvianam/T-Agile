a06b70eefb0404280b049677d5782c606398caac
wesley <wesley.vmartins.js@gmail.com>
2023-07-19 17:40:53 -0300

    Update register page

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -7,30 +7,26 @@
         <div class="shadow w-1/2 p-6 rounded-xl">
             <div>
                 <h1 class="mb-3 text-4xl font-bold text-center">Register</h1>
-                <form>
+                <form action="{{ route('register') }}" method="POST">
+                    @csrf
                     <div class="mb-3">
-                        <label for="website-admin" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Name</label>
-                        <input type="text" id="website-admin" class="rounded-lg bg-gray-50 text-gray-900 focus:border-none block flex-1 min-w-0 w-full text-sm p-2.5  dark:bg-gray-700" placeholder="Name">
+                        <label for="name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Name</label>
+                        <input type="text" id="name" name="name" class="rounded-lg bg-gray-50 text-gray-900 focus:border-none block flex-1 min-w-0 w-full text-sm p-2.5  dark:bg-gray-700" placeholder="Name">
                     </div>
 
                     <div class="mb-3">
-                        <label for="website-admin" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Username</label>
-                        <input type="text" id="website-admin" class="rounded-lg bg-gray-50 text-gray-900 focus:border-none block flex-1 min-w-0 w-full text-sm p-2.5  dark:bg-gray-700" placeholder="Email">
+                        <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Email</label>
+                        <input type="text" name="email" id="email" class="rounded-lg bg-gray-50 text-gray-900 focus:border-none block flex-1 min-w-0 w-full text-sm p-2.5  dark:bg-gray-700" placeholder="Email">
                     </div>
 
                     <div class="mb-3">
-                        <label for="website-admin" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Email</label>
-                        <input type="text" id="website-admin" class="rounded-lg bg-gray-50 text-gray-900 focus:border-none block flex-1 min-w-0 w-full text-sm p-2.5  dark:bg-gray-700" placeholder="Email">
-                    </div>
-
-                    <div class="mb-3">
-                        <label for="website-admin" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
-                        <input type="text" id="website-admin" class="rounded-lg bg-gray-50 text-gray-900 focus:border-none block flex-1 min-w-0 w-full text-sm p-2.5  dark:bg-gray-700" placeholder="Password">
+                        <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
+                        <input type="password" id="password" name="password" class="rounded-lg bg-gray-50 text-gray-900 focus:border-none block flex-1 min-w-0 w-full text-sm p-2.5  dark:bg-gray-700" placeholder="Password">
                     </div>
 
                     <div class="mb-5">
-                        <label for="website-admin" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Confirm Password</label>
-                        <input type="text" id="website-admin" class="rounded-lg bg-gray-50 text-gray-900 focus:border-none block flex-1 min-w-0 w-full text-sm p-2.5  dark:bg-gray-700" placeholder="Confirm Password">
+                        <label for="confirm-password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Confirm Password</label>
+                        <input type="password" name="confirm-password" id="confirm-password" class="rounded-lg bg-gray-50 text-gray-900 focus:border-none block flex-1 min-w-0 w-full text-sm p-2.5  dark:bg-gray-700" placeholder="Confirm Password">
                     </div>
 
                     <div>
